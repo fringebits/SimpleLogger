6f2933e59e0459ff4c776a64ec653034377da825
Mike Gutmann <mikeg@undeadlabs.com>
2026-09-22 18:48:39 -0700

    Added pause to console logger.

--- SimpleLogger/Logging/Handlers/ConsoleLoggerHandler.cs
+++ SimpleLogger/Logging/Handlers/ConsoleLoggerHandler.cs
@@ -9,9 +9,13 @@ namespace SimpleLogger.Logging.Handlers
 
         private readonly bool rawOutput;
 
+        private static int _pause = 0;
+
         public Logger.Level Level { get; set; } = Logger.Level.Info;
 
         public ConsoleColor TextColor { get; set; }
+
+        public static bool IsPaused => _pause > 0;
 
         public ConsoleLoggerHandler(bool rawOutput = true) 
             : this(new DefaultLoggerFormatter())
@@ -26,8 +30,23 @@ namespace SimpleLogger.Logging.Handlers
             this.loggerFormatter = loggerFormatter;
         }
 
+        public static void Pause()
+        {
+            _pause = _pause + 1;
+        }
+
+        public static void Resume()
+        {
+            _pause = Math.Min(0, _pause - 1);
+        }
+
         public void Publish(LogMessage logMessage)
         {
+            if (IsPaused)
+            {
+                return;
+            }
+
             if (logMessage.Level >= this.Level || (Logger.IsVerbose && logMessage.Level == Logger.Level.Debug))
             {
                 var msg = this.rawOutput ? logMessage.Text : this.loggerFormatter.ApplyFormat(logMessage);
